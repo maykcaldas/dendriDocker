5cbe5e83aee48f296f73177a11e632f6b77d1f24
maykcaldas <maykcaldas@gmail.com>
2019-02-27 11:18:32 -0300

    Run file implemented and automated call of the gromacs done

--- dendriDocker.py
+++ dendriDocker.py
@@ -15,19 +15,22 @@ import os
 
 
 def main():
+    startTime=time.time()
     script_dir = os.path.dirname(__file__)
     logo=open(script_dir+'/asciilogo.txt','r')
     print(logo.read()[-285:])
     print("\nIt's beeing develop by the MSSM/LabMMol laboratory at IQ/UFRJ.")
     print("More about us in our webpage: https://labmmol.iq.ufrj.br/mssm/ \n")
-    print("You need to have the topology and the coordinates files in way to use this software.")
+    print("You need to have the topology and the coordinates files as well as the gromacs and plummed installed\n")
+    print("in way to use this software.\n")
     print("Use -h to access the help menu.\n")
 
     parser=argparse.ArgumentParser(description="There is no description")
     # parser.add_argument('--coreSize', help="Number of atoms in the core block of the dendrimer")
     # parser.add_argument('--interSize', help="Number of atoms in the intermediary block of the dendrimer")
     # parser.add_argument('--terSize', help="Number of atoms in the terminal block of the dendrimer")
-    parser.add_argument('--ligand', help="PDB of the molecule to be docked", default="ligand.pdb")
+    parser.add_argument('--host', help="Name of the host molecule", default="Dendrimer")
+    parser.add_argument('--ligand', help="Name of the molecule to be docked", default="Ligand")
     parser.add_argument('--nligand', help="Number of ligand molecules to be docked", default="10")
     parser.add_argument('--atomInDend', help="Number of atoms in your model dendrimer", default=None)
     parser.add_argument('--atomInLigand', help="Number of atoms in your model ligand", default=None)
@@ -40,7 +43,7 @@ def main():
     parser.add_argument('--mdpPath', help="Path to the mdp files directory", default='./mdp')
     parser.add_argument('--nameOut', help="Output name of the system", default='System')
     parser.add_argument('--topolOut', help="Output name of the topology file", default='topol.top')
-    parser.add_argument('--dockOut', help="Output name of the docking file", default='plumed_dock.top')
+    parser.add_argument('--dockOut', help="Output name of the docking file", default='plumed_dock.dat')
     parser.add_argument('--runOut', help="Output name of the run file", default='run.sh')
     #parser.add_argument('--walls', help="Number of atoms in your model ligand")
 
@@ -53,20 +56,22 @@ def main():
             noneInArgs=True
     if noneInArgs == True:
         error("There were some undefined variables.")
-
-    #coreSize=args["coreSize"]
-    #interSize=args["interSize"]
-    #terSize=args["terSize"]
-    #ligand=args["ligand"]
-    #nligand=args["nligand"]
-
-    #os.system('ls -la')
-
-    #print(args)
+    
+    print("The used list of parameters are:\n")
+    for i in args:
+        print("{0:15s}: {1}".format(i, args[i]))
+    
+    print("\n")    
     prt=printer()
     prt.printPlumedDock(args)
     prt.printTop(args)
     prt.printSetup(args)
+
+    os.system('chmod u+x {0}'.format(args['runOut']))
+    os.system('./{0}'.format(args['runOut']))
+    os.system('rm -rf \#*')
+    
+    print("The run took: {0:.2g} seconds.\n".format(time.time()-startTime))
 
 
 def error(message):
