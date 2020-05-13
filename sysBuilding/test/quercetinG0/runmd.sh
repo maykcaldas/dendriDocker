e041e0423896e39d83079779b1a517a9cea89e31
maykcaldas <maykcaldas@gmail.com>
2020-05-13 13:01:45 -0300

    updating test dir

--- sysBuilding/test/quercetinG0/runmd.sh
+++ sysBuilding/test/quercetinG0/runmd.sh
@@ -3,7 +3,7 @@
 # Here we define some important variables.
 
 HERE=.
-PROGRAM=/home/mayk/programs/gromacs-5.1.4/install/bin/gmx
+PROGRAM=/home/kyam/Programs/gromacs-2019.4/install/bin/gmx_mpi
 WORKDIR=${HERE}
 
 TOPO=topol.top
@@ -12,102 +12,81 @@ MDP=./mdp
 cd ${HERE}
 
 
-################### BOX ######################
-
-${PROGRAM} editconf \
-        -f $HERE/PAMAM_G0_Neutral.gro \
-        -c \
-        -d 1.0 \
-        -bt cubic \
-        -o box1.gro\
-
-########## INSERT MOLECULES ##############
-${PROGRAM} insert-molecules \
-	 	 -f $WORKDIR/box1.gro \
-	 	 -ci quercetin.pdb \
-	 	 -nmol 8 \
-	 	 -o box2\
-
-################### BOX ######################
-
-${PROGRAM} editconf \
-        -f $HERE/box2.gro \
-        -c \
-        -d 0.2 \
-        -bt cubic \
-        -o box3.gro\
-
 ########## MINIMIZATION ###############
 
 ${PROGRAM} grompp \
     -f $MDP/em.mdp \
-    -c $WORKDIR/box3.gro \
+    -c $WORKDIR/dock.gro \
     -p ${TOPO} \
     -maxwarn 5 \
-    -o em1.tpr\
+    -o em.tpr\
 
 ${PROGRAM} mdrun \
-    -deffnm em1\
+    -deffnm em\
 
 
-################### BOX ######################
-
-${PROGRAM} editconf \
-        -f $HERE/em1.gro \
-        -c \
-        -d 0.2 \
-        -bt cubic \
-        -o box4.gro\
-
-################### SOLVATE ####################
-
-${PROGRAM} solvate \
-	-cp $WORKDIR/box4.gro \
-	-cs spc216.gro \
-	-p ${TOPO} \
-	-o solv.gro\
-
-##################### ION ######################
+######### EQUILIBRATION: NVT  ############
 
 ${PROGRAM} grompp \
-    -f $MDP/ion.mdp \
-	-c $WORKDIR/solv.gro \
-	-p ${TOPO} \
-	-o ion.tpr\
-
-
-echo SOL | ${PROGRAM} genion \
-	-s ${WORKDIR}/ion.tpr \
-	-p ${TOPO} \
-	-pname NA+ \
-	-nname CL- \
-	-np  0 \
-	-nn  0 \
-	-o ion.gro\
-
-########## MINIMIZATION ###############
-
-${PROGRAM} grompp \
-    -f $MDP/em.mdp \
-    -c $WORKDIR/ion.gro \
-    -p ${TOPO} \
-    -maxwarn 5 \
-    -o em2.tpr\
+         -f $MDP/nvt100.mdp \
+         -c $WORKDIR/em \
+         -p ${TOPO} \
+         -maxwarn 2 \
+         -o nvt100.tpr\
 
 ${PROGRAM} mdrun \
-    -deffnm em2\
+         -v \
+         -deffnm nvt100\
 
+######### EQUILIBRATION: NVT  ############
+
+${PROGRAM} grompp \
+         -f $MDP/nvt200.mdp \
+         -c $WORKDIR/nvt100.gro \
+         -p ${TOPO} \
+         -maxwarn 2 \
+         -o nvt200.tpr\
+
+${PROGRAM} mdrun \
+         -v \
+         -deffnm nvt200\
+
+######### EQUILIBRATION: NVT  ############
+
+${PROGRAM} grompp \
+         -f $MDP/nvt300.mdp \
+         -c $WORKDIR/nvt200.gro \
+         -p ${TOPO} \
+         -maxwarn 2 \
+         -o nvt300.tpr\
+
+${PROGRAM} mdrun \
+         -v \
+         -deffnm nvt300\
+
+######## EQUILIBRATION: NPT  ##############
+
+${PROGRAM} grompp \
+         -f $MDP/npt300.mdp \
+         -c $WORKDIR/nvt300 \
+         -p ${TOPO} \
+         -maxwarn 2 \
+         -o npt300.tpr\
+
+${PROGRAM} mdrun \
+         -v \
+         -deffnm npt300\
 
 ########## MOLECULAR DYNAMICS ###############
 
 ${PROGRAM} grompp \
-         -f $MDP/dock.mdp \
-         -c $WORKDIR/dock \
+         -f $MDP/md.mdp \
+         -c $WORKDIR/npt300.gro \
          -p ${TOPO} \
          -maxwarn 2 \
-         -o True.tpr\
+         -o md.tpr\
 
-${PROGRAM} mdrun \
-         -cpo True.cpt \
-         -deffnm True\
+mpirun -n 2 $PROGRAM mdrun \
+         -v \
+         -deffnm md\
 
